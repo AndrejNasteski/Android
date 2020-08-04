b9873fc553efc96d9f4cb5255e3d9b5d6c7ede56
Андреј Настески <anaste@live.com>
2020-08-04 15:09:06 +0200

    Initial commit

---
+++ app/src/main/java/com/example/commerz/AccountActivity.java
@@ -0,0 +1,44 @@
+package com.example.commerz;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.tabs.TabLayout;
+
+import androidx.viewpager.widget.ViewPager;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.example.commerz.ui.main.SectionsPagerAdapter;
+
+public class AccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account);
+        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
+        ViewPager viewPager = findViewById(R.id.view_pager);
+        viewPager.setAdapter(sectionsPagerAdapter);
+        TabLayout tabs = findViewById(R.id.tabs);
+        tabs.setupWithViewPager(viewPager);
+
+        int tabInt = getIntent().getExtras().getInt("access");
+        if (tabInt == 0){ // profile
+            TabLayout.Tab selected = tabs.getTabAt(0);
+            selected.select();
+        }
+        else if (tabInt == 1){
+            TabLayout.Tab selected = tabs.getTabAt(1);
+            selected.select();
+        }
+        else if (tabInt == 2 ){
+            TabLayout.Tab selected = tabs.getTabAt(2);
+            selected.select();
+        }
+    }
+}
---
+++ app/src/main/java/com/example/commerz/MainActivity.java
@@ -0,0 +1,153 @@
+package com.example.commerz;
+
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.google.android.material.navigation.NavigationView;
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private NavigationView navigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        navigationView = findViewById(R.id.nav_view);
+        Menu menu = navigationView.getMenu();
+
+
+        navigationView.setNavigationItemSelectedListener(this);
+
+        // find MenuItem you want to change
+
+        View headView = navigationView.getHeaderView(0);
+        ImageView imgProfile = headView.findViewById(R.id.imgProfile);
+        imgProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, AccountActivity.class);
+                i.putExtra("access", 0);
+                startActivity(i);
+            }
+        });
+
+        //default fragment for home
+
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.add(R.id.flMain, new HomeFragment());
+            fragmentTransaction.commit();
+
+            navigationView.setCheckedItem(R.id.nav_home);
+    }
+
+    public void setActionBarTitle(String title) {
+        getSupportActionBar().setTitle(title);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        /*
+        if (id == R.id.action_settings) {
+            return true;
+        }*/
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_home) {
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.add(R.id.flMain, new HomeFragment());
+            fragmentTransaction.commit();
+        } else if (id == R.id.nav_profile) {
+            openAccountActivity(0);
+        }
+        else if (id == R.id.nav_add) {
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.add(R.id.flMain, new NewAdFragment());
+            fragmentTransaction.commit();
+        }
+        else if (id == R.id.nav_favorite) {
+            openAccountActivity(2);
+        }
+        else if (id == R.id.nav_my_ads) {
+            openAccountActivity(1);
+        }
+        else if (id == R.id.nav_register){
+            androidx.fragment.app.FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.replace(R.id.flMain, new RegisterFragment());
+            ft.commit();
+        }
+        else if (id == R.id.nav_login){
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.replace(R.id.flMain, new LoginFragment());
+            ft.commit();
+        }
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+
+    public void openAccountActivity(int access){ // 0 profile, 1 my ads, 2 favorite
+        Intent i = new Intent(MainActivity.this, AccountActivity.class);
+        i.putExtra("access", access);
+        startActivity(i);
+    }
+
+
+
+}
